900017b73b42af2adfe4ea1fa6699b5eacc19eea
Michel Soares <168469501+UNI-MichelSoares@users.noreply.github.com>
2024-04-30 03:15:34 -0300

    Add files via upload
    Adição do arquivo calendario.c
    Esse é o arquivo principal do programa e, por enquanto, contém apenas a função responsável por exibir o calendário de um ano inteiro.

---
+++ calendario.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+void exibirCalendario(int diaDaSemana, int bissexto);
+
+int main(void) {
+  
+  return 0;
+}
+
+void exibirCalendario(int diaDaSemana, int bissexto) {
+
+  int diasEmCadaMes[12] = {31, 28 + bissexto, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  char meses[12][20] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+
+  // Último dia da semana de cada mês, começa com o valor do 1° dia da semana do ano.
+  int ultimo = diaDaSemana;
+
+  for (int i = 0; i < 12; i++) {
+    // Printa o nome do mês.
+    printf("\n\n%s\n", meses[i]);
+    printf("Dom Seg Ter Qua Qui Sex Sab\n");
+
+    // Preenche o calendário até o primeiro dia do mês.
+    for (int k = 0; k < ultimo; k++) {
+      printf("-   ");
+    }
+
+    // Printa os dias de cada mês e atualiza o último dia da semana do mês anterior.
+    for (int j = 1; j <= diasEmCadaMes[i]; j++) {
+      printf("%-2d  ", j);
+
+      // Atualiza o último dia da semana do mês anterior.
+      ultimo = (ultimo + 1) % 7;
+
+      // Quebra a linha a cada sábado (sétimo dia) ou no último dia do mês.
+      if (ultimo == 0 || j == diasEmCadaMes[i]) {
+        printf("\n");
+      }
+    }
+  }
+}
